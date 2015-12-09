c4bf8d76882cba9b0cd25562ff0f0d8bed5aae53
Anirvan Mandal <anirvan@hackerrank.com>
2015-12-09 13:19:47 +0530

    Initial Commit

---
+++ myfirstapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myfirstapp
+//
+//  Created by Anirvan Mandal on 09/12/15.
+//  Copyright © 2015 Anirvan Mandal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
